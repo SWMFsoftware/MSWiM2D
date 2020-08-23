da77ad282964cd592f4475479b8db7a7e2dbae47
TKeebler <tkeebler@umich.edu>
2020-08-22 23:11:47 -0400

    Still incomplete. Corrected interpolation order and modified I/O.

--- Interpolate_Output.f90
+++ Interpolate_Output.f90
@@ -6,7 +6,7 @@
 program interpolate_output
   
   ! Read a *.outs IDL file of type ascii/real4/real8 and a satellite file,
-  ! then interpolate the *.outs to the location/cadence from the satellite file.
+  ! then interpolate the *.outs to the trajectory from the satellite file.
 
   use ModIoUnit,     ONLY: io_unit_new
   use ModKind,       ONLY: Real4_, Real8_
@@ -20,10 +20,10 @@ program interpolate_output
   ! Trajectory File
   character(len=100)   :: line ! single line from trajectory file
   integer,allocatable  :: TrajTime_DI(:,:) ! integer times from trajectory file
-  integer,allocatable  :: TrajTime_I(:) ! simulation time from trajectory file
-  real,allocatable     :: Xyz_DI(:,:) ! positions at each timestamp
-  integer :: LineNumber ! line number in trajectory file
+  real,allocatable  :: TrajTime_I(:) ! simulation time from trajectory file
+  real,allocatable     :: Xy_DI(:,:) ! positions at each timestamp
   character(len=100) :: NameTrajFile ! name of desired trajectory file
+  integer :: LineNumber ! line number in trajectory file
   integer :: nPoints ! number of timestamps in trajectory file
   real    :: StartTime ! simulation start time
   real    :: DateTime ! timestamp in simulation time
@@ -32,6 +32,7 @@ program interpolate_output
   integer :: UnitTraj   ! iounit of trajectory file
   integer :: UnitIn     ! iounit of .outs file
   integer :: UnitInterp ! iounit of interpolated file
+  integer :: UnitQuery  ! iounit of query file
   integer :: iError     ! used for all files
 
   ! .outs File
@@ -40,18 +41,20 @@ program interpolate_output
   character(len=500) :: StringHeader
   character(len=500) :: NameVar
   integer            :: nStep, nDim, nParam, nVar
-  real(Real8_)              :: Time,PrevTime ! snapshot times
-  real(Real8_)              :: Param_I(100) ! parameters from .outs file
-  real(Real8_), allocatable :: Coord_DII(:,:), Var_VII(:,:)
-  real(Real8_), allocatable :: PrevCoord_DII(:,:), PrevVar_VII(:,:)
+  real(Real8_)              :: Time ! snapshot time
+  real(Real8_)              :: Param_I(100) ! parameters
+  real(Real8_), allocatable :: Coord_DI(:,:), Var_VI(:,:) ! coordinate and data matrices
   integer :: n_D(0:3), n1, n2, n3
-  integer :: i, TrajCounter
-  real(Real8_), allocatable :: InterpData_VI(:,:)
-  real(Real8_) :: dist
+  integer :: i, TrajTimestamp ! loop indices
+  real(Real8_), allocatable :: InterpData_VI(:,:) ! fully interpolated output
+  real(Real8_) :: InterpCoord(2) ! interpolated trajectory coordinates
+  integer :: MaxSnapshot ! limit of snapshots in .outs file
+  integer :: nSnapshots ! number of snapshots contained in .outs file
 
   ! Interpolated File
   character(len=100) :: NameInterpFile ! name of desired output file
-
+  real(Real8_),allocatable :: TimeOut_D(:)
+  real(Real8_),allocatable :: InterpCoord_DI(:,:)
   character(len=*), parameter:: NameSub = 'interpolate_output'
   !-------------------------------------------------------------------------
 
@@ -64,23 +67,18 @@ program interpolate_output
   !             #START
   !              1997 5 17 3 35 00 000 -0.368 5.275 0.0
 
-  ! Get name/type of trajectory file.
-!  write(*,'(a)') 'Input Filename (*.outs):'
-!  read(*,'(a)') NameFileIn
-!  write(*,'(a)') 'Input File Type (ascii/real4/real8):'
-!  read(*,'(a)') TypeFileIn
-!  write(*,'(a)') 'Trajectory Filename:'
-!  read(*,'(a)') NameTrajFile
-  NameFileIn = 'test_mov.outs'
-  TypeFileIn = 'ascii'
-  NameTrajFile = 'test_traj.dat'
-  NameInterpFile = 'test_output.dat'
+  UnitQuery = io_unit_new()
   UnitTraj = io_unit_new()
   UnitIn = io_unit_new()
   UnitInterp = io_unit_new()
-  StartTime = 1640995200.0 ! Can this be read from the plot file?
-  ! Note: all input will be from a file eventually.
 
+  open(unit=UnitQuery,file='query.txt',iostat=iError,status='old')
+  read(UnitQuery,'(a)',iostat=iError) NameFileIn
+  read(UnitQuery,'(a)',iostat=iError) TypeFileIn
+  read(UnitQuery,'(a)',iostat=iError) NameTrajFile
+  read(UnitQuery,'(a)',iostat=iError) NameInterpFile
+  read(UnitQuery,*,iostat=iError) StartTime
+  close(UnitQuery)
   
   !---------------------------------------------------------------------------
   !                   READ TRAJECTORY
@@ -104,7 +102,7 @@ program interpolate_output
   ! Allocate arrays to hold times/positions.
   allocate(TrajTime_DI(7,nPoints))
   allocate(TrajTime_I(nPoints))
-  allocate(Xyz_DI(3,nPoints))
+  allocate(Xy_DI(2,nPoints))
   
   ! Rewind to start of file for reading times/positions.
   rewind(unit=UnitTraj) 
@@ -114,7 +112,7 @@ program interpolate_output
      read(UnitTraj,'(a)',iostat=iError) line
      if(index(line, '#START')>0)then
         do LineNumber=1, nPoints
-           read(UnitTraj, *, iostat=iError) TrajTime_DI(:,LineNumber), Xyz_DI(:,LineNumber)
+           read(UnitTraj, *, iostat=iError) TrajTime_DI(:,LineNumber), Xy_DI(:,LineNumber)
            if(iError/=0)EXIT READFILE
 
            ! Convert integer time to simulation time.
@@ -127,16 +125,12 @@ program interpolate_output
   ! Close the trajectory file.
   close(unit=UnitTraj) 
 
-  write(*,*)'nPoints = ',nPoints
-  !write(*,*)'TrajTime_I(1) = ',TrajTime_I(1)
-  !write(*,*)'TrajTime_I(nPoints) = ',TrajTime_I(nPoints)
-
-
   
   !-------------------------------------------------------------------------------------
   !                            INTERPOLATE
-  TrajCounter = 1
-  do i=1,100
+  MaxSnapshot = 1000
+  nSnapshots = 0
+  do i=1,MaxSnapshot
      ! Read header info from .outs file.
      call read_plot_file(NameFile = NameFileIn, iUnitIn = UnitIn, &
           TypeFileIn = TypeFileIn, &
@@ -154,91 +148,83 @@ program interpolate_output
           ParamOut_I = Param_I)   ! parameters
 
      if(Time < TrajTime_I(1)) CYCLE  ! if before start of trajectory file
-     if(TrajCounter > nPoints) EXIT  ! if after end of trajectory file
+     if(Time > TrajTime_I(nPoints)) EXIT  ! if after end of trajectory file
 
      ! Create array to hold interpolated data.
      if(.not.allocated(InterpData_VI))then
-        allocate(InterpData_VI(nPoints,nVar))
-        !write(*,*)'nDim =',nDim
-        !write(*,*)'n1, n2, n3 =',n1,n2,n3
-        !write(*,*)'nOut_D =',n_D
+        allocate(TimeOut_D(MaxSnapshot), InterpData_VI(nVar,MaxSnapshot), &
+             InterpCoord_DI(2,MaxSnapshot))
      endif
-     
+
      ! Determine the shape of arrays from the header.
      !   Note: assumes no AMR/constant grid
-     if(.not. allocated(Coord_DII)) &
-          allocate(Coord_DII(nDim, n1), Var_VII(nVar, n1), &
-          PrevVar_VII(nVar, n1))
+     if(.not. allocated(Coord_DI)) &
+          allocate(Coord_DI(nDim, n1), Var_VI(nVar, n1))
 
-     ! Read the data and interpolate in time if necessary.
-     if(Time < TrajTime_I(TrajCounter))then
-        ! Remember the previous snapshot.
-        PrevTime = Time
-        call read_plot_file(NameFile=NameFileIn, iUnitIn=UnitIn, &
-             TypeFileIn = TypeFileIn, &
-             CoordOut_DI = Coord_DII, &
-             VarOut_VI = PrevVar_VII)
-        !write(*,*)'Time < TrajTime_I',Time,'<',TrajTime_I(TrajCounter)
-        CYCLE
-     elseif(Time > TrajTime_I(TrajCounter))then
-        ! Read the subsequent snapshot.
-        call read_plot_file(NameFile=NameFileIn, iUnitIn=UnitIn, &
-             TypeFileIn = TypeFileIn, &
-             CoordOut_DI = Coord_DII, &
-             VarOut_VI = Var_VII)
-
-        ! Interpolate the data in time.
-        !write(*,*)'Success... Time > TrajTime_I',Time,'>',TrajTime_I(TrajCounter)
-        dist = (Time - TrajTime_I(TrajCounter))/(Time - PrevTime)
-        Var_VII = dist*PrevVar_VII + (1-dist)*Var_VII   
-        TrajCounter = TrajCounter + 1
+     ! Read the data at the snapshot.
+     call read_plot_file(NameFile=NameFileIn, iUnitIn=UnitIn, &
+          TypeFileIn = TypeFileIn, &
+          CoordOut_DI = Coord_DI, &
+          VarOut_VI = Var_VI)
+     
+     ! Interpolate location of trajectory file in time to snapshot.
+     TrajTimestamp = 1
+     do while(Time > TrajTime_I(TrajTimestamp))
+        TrajTimestamp = TrajTimestamp + 1
+     enddo
+     if(Time == TrajTime_I(TrajTimestamp))then
+        InterpCoord = Xy_DI(:,TrajTimestamp)
      else
-        ! Matching snapshot: no temporal interpolation needed.
-        call read_plot_file(NameFile=NameFileIn, iUnitIn=UnitIn, &
-             TypeFileIn = TypeFileIn, &
-             CoordOut_DI = Coord_DII, &
-             VarOut_VI = Var_VII)
-        !write(*,*)'Success... Time = TrajTime_I',Time,'=',TrajTime_I(TrajCounter)
-        TrajCounter = TrajCounter + 1
+        InterpCoord = interpolate_vector( &
+             a_VC = Xy_DI(:,TrajTimestamp-1:TrajTimestamp), &
+             nVar = 2, &
+             nDim = 1, &
+             Min_D = [1], &
+             Max_D = [2], &
+             x_D = [Time], &
+             x1_I = TrajTime_I(TrajTimestamp-1:TrajTimestamp), &
+             DoExtrapolate = .false.)
      endif
-
-     ! Interpolate the data in space.
-     !Var_VII = interpolate_vector( &
-     !     a_VC = [PrevVar_VII, Var_VII], &
-     !     nVar = nVar, &
-     !     nDim = 1, &
-     !     Min_D = [1], &
-     !     Max_D = [2], &
-     !     x_D = [real(TrajTime_I(TrajCounter))], &
-     !     !x1_I = TrajTime_I(TrajCounter), &
-     !     DoExtrapolate = .false., &
-     !     iCell_D = [1], &
-     !     Dist_D = [dist])
      
+     ! Interpolate snapshot to trajectory location.
+     InterpData_VI(:,i) = interpolate_vector( &
+          a_VC = Var_VI, &
+          nVar = nVar, &
+          nDim = 2, &
+          Min_D = [1,1], &
+          Max_D = [100,100], & ! This part is incorrect. Can it be read from .outs file?
+          x_D = InterpCoord, &
+          x1_I = Coord_DI(1,:), &
+          x2_I = Coord_DI(2,:), &
+          DoExtrapolate = .false.)
+
+     InterpCoord_DI(:,i) = InterpCoord ! possibly incorporate into above statements
+     TimeOut_D(i) = Time
+
+     nSnapshots = i ! Update number of snapshots in .outs file
   enddo
-     
+  
+
   close(UnitIn)
-  deallocate(Coord_DII, Var_VII)
+  deallocate(Coord_DI, Var_VI)
   
-
-  
-
   !-------------------------------------------------------------------------------------
-  !                           WRITE OUTPUT
-  
+  !                              OUTPUT FILE
   ! Open file for interpolated output.
   open(unit=UnitInterp, file=NameInterpFile, iostat=iError, status="replace") 
 
   ! Include header.
-  !   Eventually, will need labels for time and variables.
-  write(UnitInterp,*)'Header'
-  write(UnitInterp,*)NameVar
-  
-  do LineNumber=1,nPoints
-     write(UnitInterp,'(1X,I4,6I2)')TrajTime_DI(:,LineNumber)
+  write(UnitInterp,*)'t ',NameVar
+
+  ! Write data. For now, time is in simulation time, but can be easily converted.
+  do LineNumber=1,nSnapshots
+     write(UnitInterp,*)TimeOut_D(LineNumber),InterpCoord_DI(:,LineNumber),InterpData_VI(:,LineNumber)
   enddo
   
   close(UnitInterp)
+
+
+
   
 end program interpolate_output
 
