695237acce1a4417577d32e3ebb005c51697b08c
TKeebler <tkeebler@umich.edu>
2020-08-23 21:15:41 -0400

    interpolation now works for 2D spherical_lnr grid

--- Interpolate_Output.f90
+++ Interpolate_Output.f90
@@ -9,31 +9,30 @@ program interpolate_output
   ! then interpolate the *.outs to the trajectory from the satellite file.
 
   use ModIoUnit,     ONLY: io_unit_new
-  use ModKind,       ONLY: Real4_, Real8_
-  use ModPlotFile,   ONLY: save_plot_file, read_plot_file
-  use ModUtilities,  ONLY: split_string
+  use ModPlotFile,   ONLY: read_plot_file
   use ModTimeConvert,ONLY: time_int_to_real
-  use ModInterpolate
+  use ModConst,      ONLY: cRadToDeg
+  use ModInterpolate,ONLY: interpolate_vector
   
   implicit none
 
   ! Trajectory File
-  character(len=100)   :: line ! single line from trajectory file
-  integer,allocatable  :: TrajTime_DI(:,:) ! integer times from trajectory file
-  real,allocatable  :: TrajTime_I(:) ! simulation time from trajectory file
-  real,allocatable     :: Xy_DI(:,:) ! positions at each timestamp
-  character(len=100) :: NameTrajFile ! name of desired trajectory file
-  integer :: LineNumber ! line number in trajectory file
+  character(len=100)   :: StringTraj ! single line from trajectory file
+  integer, allocatable :: iTrajTime_DI(:,:) ! integer times
+  real, allocatable    :: TrajTime_I(:) ! simulation time from trajectory file
+  real, allocatable    :: Xy_DI(:,:) ! positions at each timestamp
+  character(len=100)   :: NameTrajFile ! name of desired trajectory file
+  integer :: iLine ! line number in trajectory file
   integer :: nPoints ! number of timestamps in trajectory file
   real    :: StartTime ! simulation start time
   real    :: DateTime ! timestamp in simulation time
   
   ! I/O
-  integer :: UnitTraj   ! iounit of trajectory file
-  integer :: UnitIn     ! iounit of .outs file
-  integer :: UnitInterp ! iounit of interpolated file
-  integer :: UnitQuery  ! iounit of query file
-  integer :: iError     ! used for all files
+  integer :: iUnitTraj   ! iounit of trajectory file
+  integer :: iUnitIn     ! iounit of .outs file
+  integer :: iUnitInterp ! iounit of interpolated file
+  integer :: iUnitQuery  ! iounit of query file
+  integer :: iError      ! used for all files
 
   ! .outs File
   character(len=100) :: NameFileIn
@@ -42,20 +41,21 @@ program interpolate_output
   character(len=500) :: NameVar
   integer            :: nStep, nDim, nParam, nVar
   logical            :: IsCartesian
-  real(Real8_)              :: Time ! snapshot time
-  real(Real8_)              :: Param_I(100) ! parameters
-  real(Real8_), allocatable :: Coord_DII(:,:,:), Var_VII(:,:,:) ! coordinate and data matrices
+  real              :: Time ! snapshot time
+  real              :: Param_I(100) ! parameters
+  real, allocatable :: Coord_DII(:,:,:), Var_VII(:,:,:)
+  real, allocatable :: LogCoord_DII(:,:,:)
   integer :: n_D(0:3), n1, n2, n3
-  integer :: i, TrajTimestamp ! loop indices
-  real(Real8_), allocatable :: InterpData_VI(:,:) ! fully interpolated output
-  real(Real8_) :: InterpCoord(2) ! interpolated trajectory coordinates
+  integer :: i, iTrajTimestamp ! loop indices
+  real, allocatable :: InterpData_VI(:,:) ! fully interpolated output
+  real :: InterpCoord_D(2) ! interpolated trajectory coordinates
   integer :: MaxSnapshot ! limit of snapshots in .outs file
   integer :: nSnapshots ! number of snapshots contained in .outs file
 
   ! Interpolated File
   character(len=100) :: NameInterpFile ! name of desired output file
-  real(Real8_),allocatable :: TimeOut_D(:)
-  real(Real8_),allocatable :: InterpCoord_DI(:,:)
+  real,allocatable :: TimeOut_D(:)
+  real,allocatable :: InterpCoord_DI(:,:)
   character(len=*), parameter:: NameSub = 'interpolate_output'
   !-------------------------------------------------------------------------
 
@@ -68,32 +68,32 @@ program interpolate_output
   !             #START
   !              1997 5 17 3 35 00 000 -0.368 5.275 0.0
 
-  UnitQuery = io_unit_new()
-  UnitTraj = io_unit_new()
-  UnitIn = io_unit_new()
-  UnitInterp = io_unit_new()
+  iUnitQuery = io_unit_new()
+  iUnitTraj = io_unit_new()
+  iUnitIn = io_unit_new()
+  iUnitInterp = io_unit_new()
 
-  open(unit=UnitQuery,file='query.txt',iostat=iError,status='old')
-  read(UnitQuery,'(a)',iostat=iError) NameFileIn
-  read(UnitQuery,'(a)',iostat=iError) TypeFileIn
-  read(UnitQuery,'(a)',iostat=iError) NameTrajFile
-  read(UnitQuery,'(a)',iostat=iError) NameInterpFile
-  read(UnitQuery,*,iostat=iError) StartTime
-  close(UnitQuery)
+  open(unit=iUnitQuery,file='query.txt',iostat=iError,status='old')
+  read(iUnitQuery,'(a)',iostat=iError) NameFileIn
+  read(iUnitQuery,'(a)',iostat=iError) TypeFileIn
+  read(iUnitQuery,'(a)',iostat=iError) NameTrajFile
+  read(iUnitQuery,'(a)',iostat=iError) NameInterpFile
+  read(iUnitQuery,*,iostat=iError) StartTime
+  close(iUnitQuery)
   
   !---------------------------------------------------------------------------
   !                   READ TRAJECTORY
   
   ! Open the trajectory file.
-  open(unit=UnitTraj, file=NameTrajFile, iostat=iError, status="old") 
+  open(unit=iUnitTraj, file=NameTrajFile, iostat=iError, status="old") 
 
   ! Find the number of points in the trajectory file.
   nPoints = 0
   COUNTPOINTS: do
-     read(UnitTraj,'(a)',iostat=iError) line
-     if(index(line, '#START')>0)then
+     read(iUnitTraj,'(a)',iostat=iError) StringTraj
+     if(index(StringTraj, '#START')>0)then
         do
-           read(UnitTraj, '(a)', iostat=iError) line
+           read(iUnitTraj, '(a)', iostat=iError) StringTraj
            if(iError/=0)EXIT COUNTPOINTS
            nPoints = nPoints + 1
         enddo
@@ -101,39 +101,39 @@ program interpolate_output
   enddo COUNTPOINTS
 
   ! Allocate arrays to hold times/positions.
-  allocate(TrajTime_DI(7,nPoints))
+  allocate(iTrajTime_DI(7,nPoints))
   allocate(TrajTime_I(nPoints))
   allocate(Xy_DI(2,nPoints))
   
   ! Rewind to start of file for reading times/positions.
-  rewind(unit=UnitTraj) 
+  rewind(unit=iUnitTraj) 
 
   ! Read in the trajectory times/positions.
   READFILE: do
-     read(UnitTraj,'(a)',iostat=iError) line
-     if(index(line, '#START')>0)then
-        do LineNumber=1, nPoints
-           read(UnitTraj, *, iostat=iError) TrajTime_DI(:,LineNumber), Xy_DI(:,LineNumber)
+     read(iUnitTraj,'(a)',iostat=iError) StringTraj
+     if(index(StringTraj, '#START')>0)then
+        do iLine=1, nPoints
+           read(iUnitTraj, *, iostat=iError) iTrajTime_DI(:,iLine), &
+                Xy_DI(:,iLine)
            if(iError/=0)EXIT READFILE
 
            ! Convert integer time to simulation time.
-           call time_int_to_real(TrajTime_DI(:,LineNumber), DateTime)
-           TrajTime_I(LineNumber) = DateTime - StartTime
+           call time_int_to_real(iTrajTime_DI(:,iLine), DateTime)
+           TrajTime_I(iLine) = DateTime - StartTime
         enddo
      endif
   enddo READFILE
   
   ! Close the trajectory file.
-  close(unit=UnitTraj) 
+  close(unit=iUnitTraj) 
 
-  
-  !-------------------------------------------------------------------------------------
+  !------------------------------------------------------------------------
   !                            INTERPOLATE
   MaxSnapshot = 1000
   nSnapshots = 0
   do i=1,MaxSnapshot
      ! Read header info from .outs file.
-     call read_plot_file(NameFile = NameFileIn, iUnitIn = UnitIn, &
+     call read_plot_file(NameFile = NameFileIn, iUnitIn = iUnitIn, &
           TypeFileIn = TypeFileIn, &
           StringHeaderOut = StringHeader, &
           NameVarOut = NameVar, &
@@ -149,8 +149,6 @@ program interpolate_output
           nOut_D = n_D, &         ! nOut_D grid size array
           ParamOut_I = Param_I)   ! parameters
 
-     !write(*,*)'n1,n2,n3',n1,n2,n3
-     
      if(Time < TrajTime_I(1)) CYCLE  ! if before start of trajectory file
      if(Time > TrajTime_I(nPoints)) EXIT  ! if after end of trajectory file
 
@@ -163,88 +161,87 @@ program interpolate_output
      ! Determine the shape of arrays from the header.
      !   Note: assumes no AMR/constant grid
      if(.not. allocated(Coord_DII)) &
-          allocate(Coord_DII(nDim, n1, n2), Var_VII(nVar, n1, n2))
+          allocate(Coord_DII(nDim, n1, n2), &
+          Var_VII(nVar, n1, n2), &
+          LogCoord_DII(nDim,n1,n2))
 
      ! Read the data at the snapshot.
-     call read_plot_file(NameFile=NameFileIn, iUnitIn=UnitIn, &
+     call read_plot_file(NameFile=NameFileIn, iUnitIn=iUnitIn, &
           TypeFileIn = TypeFileIn, &
           CoordOut_DII = Coord_DII, &
           VarOut_VII = Var_VII)
      
      ! Interpolate location of trajectory file in time to snapshot.
-     TrajTimestamp = 1
-     do while(Time > TrajTime_I(TrajTimestamp))
-        TrajTimestamp = TrajTimestamp + 1
+     iTrajTimestamp = 1
+     do while(Time > TrajTime_I(iTrajTimestamp))
+        iTrajTimestamp = iTrajTimestamp + 1
      enddo
-     if(Time == TrajTime_I(TrajTimestamp))then
-        InterpCoord = Xy_DI(:,TrajTimestamp)
+     if(Time == TrajTime_I(iTrajTimestamp))then
+        InterpCoord_D = Xy_DI(:,iTrajTimestamp)
      else
-        InterpCoord = interpolate_vector( &
-             a_VC = Xy_DI(:,TrajTimestamp-1:TrajTimestamp), &
+        InterpCoord_D = interpolate_vector( &
+             a_VC = Xy_DI(:,iTrajTimestamp-1:iTrajTimestamp), &
              nVar = nDim, &
              nDim = 1, &
              Min_D = [1], &
              Max_D = [2], &
              x_D = [Time], &
-             x1_I = TrajTime_I(TrajTimestamp-1:TrajTimestamp), &
+             x1_I = TrajTime_I(iTrajTimestamp-1:iTrajTimestamp), &
              DoExtrapolate = .false.)
      endif
 
      ! Convert coordinates to curvilinear.
      if(.not.IsCartesian)then
-        InterpCoord = [sqrt(InterpCoord(1)**2 + InterpCoord(2)**2), &
-             atan2(InterpCoord(2), InterpCoord(1))]
+        ! Coordinates converted from [x,y] to [ln(r),phi]
+        InterpCoord_D = [log(sqrt(InterpCoord_D(1)**2 + &
+             InterpCoord_D(2)**2)), &
+             atan2(InterpCoord_D(2), InterpCoord_D(1))*cRadToDeg]
+        LogCoord_DII(1,:,:) = log(sqrt(Coord_DII(1,:,:)**2 + &
+             Coord_DII(2,:,:)**2))
+        LogCoord_DII(2,:,:) = atan2(Coord_DII(2,:,:), &
+             Coord_DII(1,:,:))*cRadToDeg
+
+        ! change domain to [0,360]
+        if(InterpCoord_D(2)<0) InterpCoord_D(2)=InterpCoord_D(2)+360
+        where(LogCoord_DII(2,:,:)<0)LogCoord_DII(2,:,:)=LogCoord_DII(2,:,:)+360
      endif
      
      ! Interpolate snapshot to trajectory location.
      InterpData_VI(:,i) = interpolate_vector( &
-          a_VC = Var_VII, &
-          nVar = nVar, &
-          nDim = 2, &
+          a_VC = Var_VII, & ! variable array
+          nVar = nVar, &    ! number of variables (11)
+          nDim = 2, &       ! number of dimensions
           Min_D = [1,1], &
           Max_D = [n1,n2], &
-          x_D = InterpCoord, &
-          x1_I = Coord_DII(1,:,1), & ! Incorrect indices?
-          x2_I = Coord_DII(2,:,1), & ! Incorrect indices?
+          x_D = InterpCoord_D, & ! desired position
+          x1_I = LogCoord_DII(1,:,1), &
+          x2_I = LogCoord_DII(2,1,:), &
           DoExtrapolate = .false.)
 
-     !if(i == 10)then
-     !   write(*,*)'InterpCoord',InterpCoord
-     !   write(*,*)'R:  '
-     !   write(*,*)minval(Coord_DII(1,:,1)),maxval(Coord_DII(1,:,1))
-     !   write(*,*)'Phi:  '
-     !   write(*,*)minval(Coord_DII(2,1,:)),maxval(Coord_DII(2,1,:))
-     !   !write(*,*)Coord_DII(2,:,1)
-     !   write(*,*)minval(Coord_DII(1,1,:)),maxval(Coord_DII(1,1,:))
-     !   write(*,*)Coord_DII(1,1,:)
-     !endif
-
-     InterpCoord_DI(:,i) = InterpCoord ! possibly incorporate into above statements
+     InterpCoord_DI(:,i) = InterpCoord_D
      TimeOut_D(i) = Time
 
      nSnapshots = i ! Update number of snapshots in .outs file
   enddo
   
-
-  close(UnitIn)
+  close(iUnitIn)
   deallocate(Coord_DII, Var_VII)
   
-  !-------------------------------------------------------------------------------------
+  !----------------------------------------------------------------------------
   !                              OUTPUT FILE
   ! Open file for interpolated output.
-  open(unit=UnitInterp, file=NameInterpFile, iostat=iError, status="replace") 
+  open(unit=iUnitInterp, file=NameInterpFile, iostat=iError, status="replace") 
 
   ! Include header.
-  write(UnitInterp,*)'t ',NameVar
+  write(iUnitInterp,*)'t ',NameVar
 
-  ! Write data. For now, time is in simulation time, but can be easily converted.
-  do LineNumber=1,nSnapshots
-     write(UnitInterp,*)TimeOut_D(LineNumber),InterpCoord_DI(:,LineNumber),InterpData_VI(:,LineNumber)
+  ! Write data. For now, time is in simulation time.
+  do iLine=1,nSnapshots
+     write(iUnitInterp,*)TimeOut_D(iLine), &
+          InterpCoord_DI(:,iLine), InterpData_VI(:,iLine)
   enddo
   
-  close(UnitInterp)
-
-
+  close(iUnitInterp)
 
   
 end program interpolate_output
