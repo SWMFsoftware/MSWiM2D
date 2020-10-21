9610f88233baefcfb008f647ca853f30d2f83e4d
TKeebler <tkeebler@umich.edu>
2020-10-21 13:18:15 -0400

    fixed bugs; removed testing code

--- Scripts/SelectTrajectory.py
+++ Scripts/SelectTrajectory.py
@@ -19,30 +19,36 @@ spice.furnsh('{}/junoKern.txt'.format(kernel_dir))
 
 # Set defaults.
 satellite = 'EARTH'
-start_in  = '2000/01/01'
-end_in  = '2000/02/01'
+startdate_in  = '2000/01/01'
+enddate_in  = '2000/02/01'
+starttime_in = 1
+endtime_in = 1
 interval = 'day'
 
 # Read input parameters from file.
 infile = open('{}/TrajectoryParameters.txt'.format(mswim2d_dir),'r')
 lines = infile.readlines()
 for line in lines:
-    if('Satellite Name:' in line):satellite = line.split(':')[-1].upper().strip()
-    if('Start' in line):start_in = line.split(':')[-1].strip()
-    if('End' in line):end_in = line.split(':')[-1].strip()
-    if('Resolution' in line):interval = line.split(':')[-1].strip()
+    if('Satellite Name:' in line): satellite = line.split(':')[-1].upper().strip()
+    if('Start Date' in line): startdate_in = line.split(':')[-1].strip()
+    if('End Date' in line):   enddate_in   = line.split(':')[-1].strip()
+    if('Start Time' in line): starttime_in = line.split('):')[-1].strip()
+    if('End Time' in line):   endtime_in   = line.split('):')[-1].strip()
+    if('Resolution' in line): interval = line.split(':')[-1].strip()
 infile.close()
 
 # Construct start and end times.
-start_time = dt.datetime(int(start_in[:4]), int(start_in[5:7]), int(start_in[-2:]))
-end_time = dt.datetime(int(end_in[:4]), int(end_in[5:7]), int(end_in[-2:]))
+start_time = dt.datetime(int(startdate_in[:4]), int(startdate_in[5:7]), int(startdate_in[-2:]),
+                         int(starttime_in[:2]), int(starttime_in[3:5]), int(starttime_in[6:]))
+end_time = dt.datetime(int(enddate_in[:4]), int(enddate_in[5:7]), int(enddate_in[-2:]),
+                       int(endtime_in[:2]), int(endtime_in[3:5]), int(endtime_in[6:]))
 
 # Create target name for SPICE.
 planets_list=['EARTH','MARS','JUPITER','SATURN','URANUS','NEPTUNE','PLUTO']
-satellites_dict={'CASSINI':[dt.datetime(2002,4,11),dt.datetime(2008,8,8)],
+satellites_dict={'CASSINI':[dt.datetime(1902,3,30),dt.datetime(2038,8,8)],
                  'VOYAGER 1':[dt.datetime(1977,9,6),dt.datetime(2030,12,31)],
                  'VOYAGER 2':[dt.datetime(1977,8,21),dt.datetime(2030,12,31)],
-                 'JUNO':[dt.datetime(2011,8,6),dt.datetime(2017,10,16)],
+                 'JUNO':[dt.datetime(2011,8,6),dt.datetime(2020,8,22)],
                  'ULYSSES':[dt.datetime(1990,10,7),dt.datetime(2050,1,1)],
                  'STEREO':[dt.datetime(2006,10,26),dt.datetime(2050,1,1)],
                  'NEW HORIZONS': [dt.datetime(2015, 1, 1), dt.datetime(2015, 2, 1)],}
@@ -58,10 +64,11 @@ if(satellite=='VOYAGER 1' and end_time > dt.datetime(1981,8,1)):
     print('Warning: Voyager I leaves +/- 6 lat HGI in 1981/08.')
 
 # Check to see if times match available data for satellite.
-if(start_time < satellites_dict[satellite][0] or
-        end_time > satellites_dict[satellite][1]):
-    exit('Error: Data only available from {} to {}.'.format(satellites_dict[satellite][0].strftime('%Y/%m/%d'),
-                                                            satellites_dict[satellite][1].strftime('%Y/%m/%d')))
+if(satellite in satellites_dict.keys()):
+    if(start_time < satellites_dict[satellite][0] or
+            end_time > satellites_dict[satellite][1]):
+        exit('Error: Data only available from {} to {}.'.format(satellites_dict[satellite][0].strftime('%Y/%m/%d'),
+                                                                satellites_dict[satellite][1].strftime('%Y/%m/%d')))
 
 # Construct array of timestamps based on provided endpoints and desired interval.
 target_times = list()
@@ -103,21 +110,3 @@ for i,time in enumerate(target_times):
     sat_file.write('{:.3f} {:.3f} 0.0\n'.format(positions[0][i],positions[1][i]))
 sat_file.close()
 
-
-
-
-################################## TESTING #######################################
-
-# r, phi = list(),list()
-# for i,entry in enumerate(positions[0]):
-#     r.append(np.sqrt(positions[1][i]**2+positions[0][i]**2))
-#     phi.append(np.degrees(math.atan2(positions[1][i],positions[0][i])))
-# print('X: ',positions[0][0],positions[0][-1])
-# print('Y: ',positions[1][0],positions[1][-1])
-# print('R: ',r[0],r[-1])
-# print('Phi: ',phi[0],phi[-1])
-plottraj = False
-if(plottraj):
-    plt.plot(positions[0],positions[1])
-    plt.show()
-    plt.close()
